da0ac71f82360e9311965ddf0a8a74f5b796a865
BENthedude425 <unconfigured@null.spigotmc.org>
2024-10-05 13:51:27 +0100

    added spigot

--- webscraper.js
+++ webscraper.js
@@ -2,7 +2,8 @@ const puppeteer = require("puppeteer");
 const { writeFileSync } = require("fs");
 const { sleep } = require("deasync");
 
-const STARTTIME = Date.now()
+const STARTTIME = Date.now();
+const THREADCOUNT = 15
 
 URLS = {
     vanilla: "https://mcversions.net/",
@@ -11,10 +12,10 @@ URLS = {
 };
 
 async function GetVanillaSources() {
-    const page = await browser.newPage();
+    const page = await browser.newPage()
     await page.goto(URLS["vanilla"]);
 
-    let urls = await page.evaluate(() => {
+    let urls = await page.evaluate((URLS) => {
         let results = [];
         let items = document.querySelectorAll("a");
 
@@ -23,14 +24,14 @@ async function GetVanillaSources() {
 
             if (link.startsWith("/download")) {
                 results.push({
-                    url: "https://mcversions.net/" + link,
+                    url: `${URLS.vanilla}${link}`,
                     version: link.slice(10, link.length),
                 });
             }
         });
 
         return results;
-    });
+    }, URLS);
 
     page.close();
     return urls;
@@ -79,55 +80,6 @@ async function GetVanillaFiles(sources, threadData) {
     threadData.completeThreads += 1;
 }
 
-async function ProcessFiles(sources, getfilecall) {
-    const STARTTIME = Date.now()
-    const threadCount = 20;
-    const x = Math.floor(sources.length / threadCount);
-    const r = sources.length % threadCount;
-
-    threadData = {
-        results: {},
-        completeThreads: 0,
-    };
-
-    // Populate the object with each version in order
-    for (let i = 0; i < sources.length; i++) {
-        const selectedSource = sources[i];
-        threadData.results[selectedSource.version] = "";
-    }
-
-    for (let i = 0; i < threadCount; i++) {
-        const start = i * x;
-        var end = start + x + 1;
-
-        if (i == threadCount - 1) {
-            end += r;
-        }
-
-        const selectedSources = sources.slice(start, end);
-        getfilecall(selectedSources, threadData);
-    }
-
-    while (threadData.completeThreads != threadCount) {
-        await sleep(1);
-    }
-
-    console.log("Cleaning up...\n");
-
-    const keys = Object.keys(threadData.results);
-    for (let i = 0; i < keys.length; i++) {
-        const selectedKey = keys[i];
-        if (!CheckData(threadData.results[selectedKey])) {
-            delete threadData.results[selectedKey];
-        }
-    }
-
-    const timeTaken = (Date.now() - STARTTIME) / 1000
-    console.log(`Took ${timeTaken}s`)
-
-    return threadData.results;
-}
-
 async function GetForgeSources() {
     const page = await browser.newPage();
     await page.goto(URLS["forge"]);
@@ -152,6 +104,7 @@ async function GetForgeSources() {
         return sources;
     }, URLS);
 
+    page.close()
     return urls;
 }
 
@@ -194,7 +147,7 @@ async function GetForgeFiles(sources, threadData) {
 
                 // Add found release to the object
                 links.push({
-                    release: fileRelease,
+                    file: fileRelease,
                     link: link,
                 });
 
@@ -209,6 +162,120 @@ async function GetForgeFiles(sources, threadData) {
 
     page.close();
     threadData.completeThreads += 1;
+}
+
+async function GetSpigotSources() {
+    const page = await browser.newPage();
+    await page.goto(URLS.spigot);
+
+    const sources = await page.evaluate(() => {
+        const items = document.querySelectorAll("div");
+        var downloads = [];
+
+        for (let i = 0; i < items.length; i++) {
+            const selectedItem = items[i];
+
+            if (selectedItem.className != "row vdivide") {
+                continue;
+            }
+
+            const link = selectedItem.children[3].children[1].children[0].href;
+            const version = selectedItem.children[0].children[1].innerText;
+
+            downloads.push({
+                url: link,
+                version: version,
+            });
+        }
+
+        return downloads;
+    });
+
+    page.close();
+    return sources;
+}
+
+async function GetSpigotFiles(sources, threadData) {
+    const page = await browser.newPage();
+
+    for (let i = 0; i < sources.length; i++) {
+        const selectedSource = sources[i];
+
+        await page.goto(selectedSource.url);
+
+        const result = await page.evaluate(() => {
+            const items = document.querySelectorAll("a");
+
+            for (let x = 0; x < items.length; x++) {
+                const selectedItem = items[x];
+
+                if (!selectedItem.innerText.endsWith(".jar")) {
+                    continue;
+                }
+
+                const link = selectedItem.href;
+                const fileName = selectedItem.innerText;
+
+                return {
+                    file: fileName,
+                    link: link,
+                };
+            }
+        });
+
+        threadData.results[selectedSource.version] = result;
+    }
+
+    page.close();
+    threadData.completeThreads += 1;
+}
+
+async function ProcessFiles(sources, getfilecall) {
+    const STARTTIME = Date.now();
+    const x = Math.floor(sources.length / THREADCOUNT);
+    const r = sources.length % THREADCOUNT;
+
+    threadData = {
+        results: {},
+        completeThreads: 0,
+    };
+
+    // Populate the object with each version in order
+    for (let i = 0; i < sources.length; i++) {
+        const selectedSource = sources[i];
+        threadData.results[selectedSource.version] = "";
+    }
+
+    for (let i = 0; i < THREADCOUNT; i++) {
+        const start = i * x;
+        var end = start + x + 1;
+
+        if (i == THREADCOUNT - 1) {
+            end += r;
+        }
+
+        const selectedSources = sources.slice(start, end);
+        getfilecall(selectedSources, threadData);
+    }
+
+    while (threadData.completeThreads != THREADCOUNT) {
+        await sleep(1);
+    }
+
+    console.log("Cleaning up...\n");
+
+    const keys = Object.keys(threadData.results);
+    for (let i = 0; i < keys.length; i++) {
+        const selectedKey = keys[i];
+        if (!CheckData(threadData.results[selectedKey])) {
+            delete threadData.results[selectedKey];
+        }
+    }
+
+    const timeTaken = (Date.now() - STARTTIME) / 1000;
+    console.log(`Took ${timeTaken}s`);
+
+    return threadData.results;
 }
 
 function CheckData(data) {
@@ -230,26 +297,28 @@ async function main() {
     const vanillaSources = await GetVanillaSources();
     const vanillaFiles = await ProcessFiles(vanillaSources, GetVanillaFiles);
 
-    //writeFileSync("text2.json", JSON.stringify(vanillaSources, "", "    "));
-
     console.log("Gathering forge server files..");
     const forgeSources = await GetForgeSources();
     const forgeFiles = await ProcessFiles(forgeSources, GetForgeFiles);
 
+    console.log("Gathering spigot server files..");
+    const spigotSources = await GetSpigotSources();
+    const spigotFiles = await ProcessFiles(spigotSources, GetSpigotFiles);
+
+    // Add all files collected into a signle object
     const allFiles = {
         Vanilla: vanillaFiles,
         Forge: forgeFiles,
+        Spigot: spigotFiles,
     };
 
-    await writeFileSync("text.json", JSON.stringify(allFiles, "", "    "));
+    await writeFileSync("Output.json", JSON.stringify(allFiles, "", "    "));
 
     console.log("all data has been written to file");
     browser.close();
 
-    console.log(Date.now(), STARTTIME)
-    
-    const timeTaken = (Date.now() - STARTTIME) / 1000
-    console.log(`Collected ${allFiles.length} items in ${timeTaken}s`)
+    const timeTaken = (Date.now() - STARTTIME) / 1000;
+    console.log(`Collected ${allFiles.length} items in ${timeTaken}s`);
 }
 
 main();
